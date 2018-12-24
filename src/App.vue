3b7d5e6f4414f5d25f20416d25287dfd4137686f
Memo Chou <memochou1993@gmail.com>
2018-12-24 23:21:38 +0800

    新的提交

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <v-app>
+      <Toolbar />
+      <v-content>
+        <router-view/>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Toolbar from '@/components/Toolbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Toolbar,
+  },
+  data() {
+    return {
+      //
+    };
+  },
+};
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-toolbar app>
+      <v-toolbar-title
+        class="headline font-weight-light"
+      >
+      竹北高中動畫社典藏庫
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-btn
+          flat
+          href="#"
+          class="title font-weight-light"
+        >
+          回首頁
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<style scoped lang="stylus">
+.headline, .title
+  font-family 'Microsoft Jhenghei' !important
+</style>
