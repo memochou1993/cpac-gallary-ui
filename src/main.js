8eef58df96c2383ce807f677c96517143d91b0f9
Memo Chou <memochou1993@gmail.com>
2018-12-27 21:34:01 +0800

    修改字型引入位置

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-// import 'roboto-fontface/css/roboto/roboto-fontface.css';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import './stylus/main.styl';
 
 Vue.config.productionTip = false;
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import zhHant from 'vuetify/es5/locale/zh-Hant';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.use(Vuetify, {
