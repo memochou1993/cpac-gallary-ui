9802af4361f70890a5fcfc8625c515590b34656e
Memo Chou <memochou1993@gmail.com>
2018-12-28 00:36:45 +0800

    新增屆別選擇

---
+++ src/components/AppGroup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-select
+      :label="label"
+      :items="groups"
+      :noDataText="noDataText"
+      solo
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      noDataText: '',
+      label: '屆別',
+      groups: [
+        '第1屆',
+        '第2屆',
+        '第3屆',
+      ],
+    };
+  },
+};
+</script>
